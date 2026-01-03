2c1e5d85999e6c20b967ae51838a9760f9dfcbc9
NxtFi Innovations Inc <nxtfiinnovationsinc@gmail.com>
2026-01-03 14:42:03 +0000

    ok

---
+++ hardhat.config.ts
@@ -0,0 +1,18 @@
+import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+import { HardhatUserConfig } from "hardhat/config";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.19",
+  networks: {
+    bscTestnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+    }
+  }
+};
+
+export default config;
